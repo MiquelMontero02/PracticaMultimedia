ed70914083983220c7c06f6296bfe40c5c590618
MiquelMontero02 <miquelangelmontero@gmail.com>
2024-03-16 18:58:04 +0100

    PORFIN

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,131 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-document.addEventListener('DOMContentLoaded', () => {
+//cargarContenidoOriginal();
+function borrar(e){
+  var cont=e.t
+}
+
+function cargarContenidoEspecifico(event) {
+  //Tornam s'enllaç com era abans
+  event.preventDefault();
+  var contentType="vaques"
+  //Guardam es main que hi era abans
+  var originalMainContent = document.getElementById('main').innerHTML;
+  var mainContent = document.getElementById('main');
+  if (contentType === "vaques") {
+    mainContent.innerHTML =
+      `<div class="container" data-aos="fade-up">
+  <div class="section-header">
+    <h2>Calendari</h2>
+    <p>Fira de ses vaques  
+        <br><span>Campos</span>
+    </p>
+  </div>
+  <div class="row gy-4">
+    
+      <div class="content ps-0 ps-lg-5">
+        <p class="fst-italic">
+          Tornen les vaques a Campos, torna la Fira i torna el més autèntic ambient tradicional mallorquí el 13 i 14 de maig. La Fira de les Seves Vaques omplirà novament els carrers del poble amb totes les seves propostes, que estan recollides al programa elaborat per l'Ajuntament.
+          El segon cap de setmana de maig les millors ramaderies acudeixen a participar al Concurs Morfològic Nacional Selecte de bestiar boví frisó, una cita ineludible en la qual les vaques passegen pel cèntric carrer de Sus Rondes perquè la gent del poble i els turistes les puguin veure.
+        </p>
+        <section id="maps" class="mb-3">
+    <div class="container" data-aos="fade-up">
+      <iframe style="border:0; width: 100%; height: 350px;"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+        frameborder="0" allowfullscreen></iframe>
+    </div>
+  </section><!-- End Contact Section -->
+
+        <button id="backButton">Volver atrás</button>
+      
+    </div>
+  </div>
+</div>`;
+  } else if (contentType === "vermar") {
+    // Contenido para Fira des Vermar
+    mainContent.innerHTML = `<div class="container" data-aos="fade-up">
+  <div class="section-header">
+    <h2>Calendari</h2>
+    <p>Fira de ses vaques  
+        <br><span>Campos</span>
+    </p>
+  </div>
+  <div class="row gy-4">
+    
+      <div class="content ps-0 ps-lg-5">
+        <p class="fst-italic">
+          Tornen les vaques a Campos, torna la Fira i torna el més autèntic ambient tradicional mallorquí el 13 i 14 de maig. La Fira de les Seves Vaques omplirà novament els carrers del poble amb totes les seves propostes, que estan recollides al programa elaborat per l'Ajuntament.
+          El segon cap de setmana de maig les millors ramaderies acudeixen a participar al Concurs Morfològic Nacional Selecte de bestiar boví frisó, una cita ineludible en la qual les vaques passegen pel cèntric carrer de Sus Rondes perquè la gent del poble i els turistes les puguin veure.
+        </p>
+        <section id="maps" class="mb-3">
+    <div class="container" data-aos="fade-up">
+      <iframe style="border:0; width: 100%; height: 350px;"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+        frameborder="0" allowfullscreen></iframe>
+    </div>
+  </section><!-- End Contact Section -->
+
+        <button id="backButton">Volver atrás</button>
+      
+    </div>
+  </div>
+</div>`;
+  } else if (contentType === "medieval") {
+    // Contenido para Fira Medieval
+    mainContent.innerHTML = `
+    <div class="container" data-aos="fade-up">
+      <div class="section-header">
+        <h2>Calendari</h2>
+        <p>Fira de ses vaques  
+            <br><span>Campos</span>
+        </p>
+      </div>
+      <div class="row gy-4">          
+          <div class="content ps-0 ps-lg-5">
+            <p class="fst-italic">
+              Tornen les vaques a Campos, torna la Fira i torna el més autèntic ambient tradicional mallorquí el 13 i 14 de maig. La Fira de les Seves Vaques omplirà novament els carrers del poble amb totes les seves propostes, que estan recollides al programa elaborat per l'Ajuntament.
+              El segon cap de setmana de maig les millors ramaderies acudeixen a participar al Concurs Morfològic Nacional Selecte de bestiar boví frisó, una cita ineludible en la qual les vaques passegen pel cèntric carrer de Sus Rondes perquè la gent del poble i els turistes les puguin veure.
+            </p>
+            <section id="maps" class="mb-3">
+              <div class="container" data-aos="fade-up">
+                <iframe style="border:0; width: 100%; height: 350px;"
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                  frameborder="0" allowfullscreen></iframe>
+              </div>
+            </section><!-- End Contact Section -->
+            <button id="backButton">Volver atrás</button>
+          </div>
+      </div>
+    </div>`;
+  }
+
+  // Asociar evento de clic al botón de "volver atrás"
+  document.getElementById('backButton').addEventListener('click', function (event) {
+    event.preventDefault();
+    // Restaurar el contenido original del main
+    cargarContenidoOriginal(originalMainContent);
+  });
+}
+function cargarContenidoOriginal(Main) {
+  var mainContent = document.getElementById('main');
+  mainContent.innerHTML = Main;
+
+  // Obtener todos los elementos con la clase "refreshButton"
+  var refreshButtons = document.querySelectorAll('.refreshButton');
+
+  // Iterar sobre los botones y agregar el evento de clic a cada uno
+  refreshButtons.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+      event.preventDefault();
+      var contentType = this.getAttribute('data-content');
+      cargarContenidoEspecifico(contentType);
+    });
+  });
+   start()
+}
+function start(){
+  
   "use strict";
 
   /**
@@ -239,131 +363,12 @@ document.addEventListener('DOMContentLoaded', () => {
     aos_init();
   });
 
-  // Función para cargar el contenido "main" específico
-  function cargarContenidoEspecifico(contentType) {
-    var mainContent = document.getElementById('mainContent');
-    if (contentType === "vaques") {
-      mainContent.innerHTML =
-        `<div class="container" data-aos="fade-up">
-    <div class="section-header">
-      <h2>Calendari</h2>
-      <p>Fira de ses vaques  
-          <br><span>Campos</span>
-      </p>
-    </div>
-    <div class="row gy-4">
-      
-        <div class="content ps-0 ps-lg-5">
-          <p class="fst-italic">
-            Tornen les vaques a Campos, torna la Fira i torna el més autèntic ambient tradicional mallorquí el 13 i 14 de maig. La Fira de les Seves Vaques omplirà novament els carrers del poble amb totes les seves propostes, que estan recollides al programa elaborat per l'Ajuntament.
-            El segon cap de setmana de maig les millors ramaderies acudeixen a participar al Concurs Morfològic Nacional Selecte de bestiar boví frisó, una cita ineludible en la qual les vaques passegen pel cèntric carrer de Sus Rondes perquè la gent del poble i els turistes les puguin veure.
-          </p>
-          <section id="maps" class="mb-3">
-      <div class="container" data-aos="fade-up">
-        <iframe style="border:0; width: 100%; height: 350px;"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-          frameborder="0" allowfullscreen></iframe>
-      </div>
-    </section><!-- End Contact Section -->
-
-          <button id="backButton">Volver atrás</button>
-        
-      </div>
-    </div>
-  </div>`;
-    } else if (contentType === "vermar") {
-      // Contenido para Fira des Vermar
-      mainContent.innerHTML = `<div class="container" data-aos="fade-up">
-    <div class="section-header">
-      <h2>Calendari</h2>
-      <p>Fira de ses vaques  
-          <br><span>Campos</span>
-      </p>
-    </div>
-    <div class="row gy-4">
-      
-        <div class="content ps-0 ps-lg-5">
-          <p class="fst-italic">
-            Tornen les vaques a Campos, torna la Fira i torna el més autèntic ambient tradicional mallorquí el 13 i 14 de maig. La Fira de les Seves Vaques omplirà novament els carrers del poble amb totes les seves propostes, que estan recollides al programa elaborat per l'Ajuntament.
-            El segon cap de setmana de maig les millors ramaderies acudeixen a participar al Concurs Morfològic Nacional Selecte de bestiar boví frisó, una cita ineludible en la qual les vaques passegen pel cèntric carrer de Sus Rondes perquè la gent del poble i els turistes les puguin veure.
-          </p>
-          <section id="maps" class="mb-3">
-      <div class="container" data-aos="fade-up">
-        <iframe style="border:0; width: 100%; height: 350px;"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-          frameborder="0" allowfullscreen></iframe>
-      </div>
-    </section><!-- End Contact Section -->
-
-          <button id="backButton">Volver atrás</button>
-        
-      </div>
-    </div>
-  </div>`;
-    } else if (contentType === "medieval") {
-      // Contenido para Fira Medieval
-      mainContent.innerHTML = `<div class="container" data-aos="fade-up">
-    <div class="section-header">
-      <h2>Calendari</h2>
-      <p>Fira de ses vaques  
-          <br><span>Campos</span>
-      </p>
-    </div>
-    <div class="row gy-4">
-      
-        <div class="content ps-0 ps-lg-5">
-          <p class="fst-italic">
-            Tornen les vaques a Campos, torna la Fira i torna el més autèntic ambient tradicional mallorquí el 13 i 14 de maig. La Fira de les Seves Vaques omplirà novament els carrers del poble amb totes les seves propostes, que estan recollides al programa elaborat per l'Ajuntament.
-            El segon cap de setmana de maig les millors ramaderies acudeixen a participar al Concurs Morfològic Nacional Selecte de bestiar boví frisó, una cita ineludible en la qual les vaques passegen pel cèntric carrer de Sus Rondes perquè la gent del poble i els turistes les puguin veure.
-          </p>
-          <section id="maps" class="mb-3">
-      <div class="container" data-aos="fade-up">
-        <iframe style="border:0; width: 100%; height: 350px;"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-          frameborder="0" allowfullscreen></iframe>
-      </div>
-    </section><!-- End Contact Section -->
-
-          <button id="backButton">Volver atrás</button>
-        
-      </div>
-    </div>
-  </div>`;
-    }
-
-    // Asociar evento de clic al botón de "volver atrás"
-    document.getElementById('backButton').addEventListener('click', function (event) {
-      event.preventDefault();
-      // Restaurar el contenido original del main
-      cargarContenidoOriginal();
-    });
-  }
-
-  // Función para cargar el contenido original del "main"
-  function cargarContenidoOriginal() {
-    var mainContent = document.getElementById('mainContent');
-    mainContent.innerHTML = originalMainContent;
-
-    // Obtener todos los elementos con la clase "refreshButton"
-    var refreshButtons = document.querySelectorAll('.refreshButton');
-
-    // Iterar sobre los botones y agregar el evento de clic a cada uno
-    refreshButtons.forEach(function (button) {
-      button.addEventListener('click', function (event) {
-        event.preventDefault();
-        var contentType = this.getAttribute('data-content');
-        cargarContenidoEspecifico(contentType);
-      });
-    });
-  }
-
-  // Guardar el contenido original del "main"
-  var originalMainContent = document.getElementById('mainContent').innerHTML;
-
-  // Cargar el contenido original por defecto
-  cargarContenidoOriginal();
+  /*Codigo Propio*/
+  var boton=document.getElementById("vaques")
+  boton.addEventListener('click',cargarContenidoEspecifico);
 
 
+}
 
+document.addEventListener('DOMContentLoaded', start);
 
-});
