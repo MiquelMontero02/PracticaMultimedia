263e41958eeaaf779a5b0233295bc06d347b382e
MiquelMontero02 <104500695+MiquelMontero02@users.noreply.github.com>
2024-03-26 17:51:43 +0100

    Arreglos y Mapita en la ferias

--- assets/js/APICall.js
+++ assets/js/APICall.js
@@ -11,22 +11,34 @@ function initMap() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Marcadores para los pueblos
+
     var pueblos = [
-        { nombre: 'Manacor', coordenadas: [39.5696, 3.2090] },
-        { nombre: 'Inca', coordenadas: [39.7219, 2.9087] },
-        { nombre: 'Campos', coordenadas: [39.4274, 3.0196] }
     ];
-    // Añadir marcadores al mapa
-    pueblos.forEach(function(pueblo) {
-        var marker=L.marker(pueblo.coordenadas)
-        var boton="<p data-content='vaques'>"+pueblo.nombre+"</p>"
-        //boton.addEventListener('click',cargarContenidoEspecifico);
-            marker.addTo(map)
-            marker.bindPopup(boton);
-            marker.openPopup();
-            marker.on('click', function(event){cargarContenidoEspecifico});
+    cargarJSONLocal('fires.json',function(evento){
+        evento.forEach(function(event){
+            var pueblo= { nombre:event.location.address.addressLocality,coordenadas:[event.location.geo.latitude,event.location.geo.longitude]};
+            pueblos.push(pueblo) 
+            var marker=L.marker(pueblo.coordenadas)
+            //boton.addEventListener('click',cargarContenidoEspecifico);
+                marker.addTo(map)
+                marker.bindPopup("<p>"+pueblo.nombre+"</p>");
+                marker.openPopup();
+                console.log(pueblo.coordenadas)     
+        });
     });
+}
+function initMapEspecific(evento) {
+    // Coordenadas centrales de las Islas Baleares
+    var foco = [evento.location.geo.latitude, evento.location.geo.longitude];
+    
+    // Inicializar el mapa
+    var map = L.map('api-map').setView(foco, 11);
+
+    // Capa de OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    console.log("Añadadido contenido de "+evento.location.address.addressLocality);
 }
 
 // Llamar a la función de inicialización del mapa cuando el DOM esté listo
