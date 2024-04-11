e82c186a324b654c03feb23b8742d5a884f3aa92
MiquelMontero02 <104500695+MiquelMontero02@users.noreply.github.com>
2024-04-11 15:38:28 +0200

    Api Arreglada

--- assets/js/APICall.js
+++ assets/js/APICall.js
@@ -19,14 +19,58 @@ function initMap() {
             var pueblo= { nombre:event.location.address.addressLocality,coordenadas:[event.location.geo.latitude,event.location.geo.longitude]};
             pueblos.push(pueblo) 
             var marker=L.marker(pueblo.coordenadas)
+            var button=document.createElement('button');
+            button.textContent=event.about;
+            button.classList.add('btn');
+            button.addEventListener('click',function(){
+                mostrarInformacionEventoEspecifico(event,document.getElementById('main').innerHTML,'#event');
+            });
             //boton.addEventListener('click',cargarContenidoEspecifico);
                 marker.addTo(map)
-                marker.bindPopup("<p>"+pueblo.nombre+"</p>");
-                marker.openPopup();
-                console.log(pueblo.coordenadas)     
+                marker.bindPopup(button); 
         });
     });
 }
+
+// Función para inicializar el mapa
+function initMapFiltrado(filtro) {
+    // Coordenadas centrales de las Islas Baleares
+    var baleares = [39.534178, 2.857710];
+    
+    // Inicializar el mapa
+    var map = L.map('api-map').setView(baleares, 8);
+
+    // Capa de OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    cargarJSONLocal('fires.json',function(evento){
+    var pueblos = [];
+    if (filtro=='fires'){
+        if(filtro=='Mallorca'){
+            pueblos=evento.filter
+        }
+        else if(filtro=='Menorca'){}
+        else if(filtro=='Eivisa'){}
+        else{}
+    }
+        evento.forEach(function(event){
+            var pueblo= { nombre:event.location.address.addressLocality,coordenadas:[event.location.geo.latitude,event.location.geo.longitude]};
+            pueblos.push(pueblo) 
+            var marker=L.marker(pueblo.coordenadas)
+            var button=document.createElement('button');
+            button.textContent=event.about;
+            button.classList.add('btn');
+            button.addEventListener('click',function(){
+                mostrarInformacionEventoEspecifico(event,document.getElementById('main').innerHTML,'#event');
+            });
+            //boton.addEventListener('click',cargarContenidoEspecifico);
+                marker.addTo(map)
+                marker.bindPopup(button); 
+        });
+    });
+}
+
 function initMapEspecific(evento) {
     // Coordenadas centrales de las Islas Baleares
     var foco = [evento.location.geo.latitude, evento.location.geo.longitude];
@@ -40,9 +84,3 @@ function initMapEspecific(evento) {
     }).addTo(map);
     console.log("Añadadido contenido de "+evento.location.address.addressLocality);
 }
-
-// Llamar a la función de inicialización del mapa cuando el DOM esté listo
-document.addEventListener("DOMContentLoaded", function() {
-    initMap();
-});
-
