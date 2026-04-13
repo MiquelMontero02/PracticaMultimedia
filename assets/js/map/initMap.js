886bc8de54cc38120a1917688ea9a915bd5784fc
MiquelMontero02 <miquelangelmontero@gmail.com>
2026-04-13 14:03:15 +0200

    IMPROVE: use the same fscript for general and specific maps

--- assets/js/map/initMap.js
+++ assets/js/map/initMap.js
@@ -1,6 +1,5 @@
-async function initMap(opc='fires') {
-      CargarLibreria();
-      const IB_LOCATION={
+async function initMap(opc='fires',data=null) {
+    const IB_LOCATION={
         'mallorca': [39.534178, 2.857710],
         'menorca': [39.96025378522197, 4.09060689266232],
         'eivissa': [38.97932847627715, 1.3768946629426013],
@@ -12,31 +11,30 @@ async function initMap(opc='fires') {
         'fires':'/assets/json/fires.json'
     }
     const url=EXSTERNAL_APIS_URL[opc];
-    const data=await fetchAPI(url)
+    
     const mapContainer = document.getElementById('api-map');
-    const isMapInitialized = mapContainer.getAttribute('data-map-initialized');
     const opcFormatedName=opc.charAt(0).toUpperCase() + opc.slice(1)
 
-    if (!isMapInitialized) {
-        const map = L.map(mapContainer).setView(IB_LOCATION['mallorca'], 8);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    addMapStylesLinks();  
+
+    const map = L.map(mapContainer).setView(IB_LOCATION['mallorca'], 8);
+    
+    L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+        {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-
+    if(!data){
+        data=await fetchAPI(url);
         data.forEach(item =>{
             const marker=addMarkers(opcFormatedName, item);
             marker.addTo(map);
         });
-
-        mapContainer.setAttribute('data-map-initialized', 'true');
-    } else {
-        // Si el mapa ya está inicializado, solo actualiza los marcadores
-        const map = L.map(mapContainer);
-        data.forEach(item =>{
-            const marker=addMarkers(opcFormatedName, item);
-            marker.addTo(map);
-        }
-        );
+    }else{
+        const marker=addMarkers(opcFormatedName, data);
+        marker.addTo(map);
     }
+    mapContainer.setAttribute('data-map-initialized', 'true');
+    
 }
