9ff96ede7a1c77ec5f6d24632449266d957a13fe
MiquelMontero02 <miquelangelmontero@gmail.com>
2026-04-14 13:34:34 +0200

    HOTFIX-Not shown temperature in Events sections

--- assets/js/meteoAPI/generateMeteoContainer.js
+++ assets/js/meteoAPI/generateMeteoContainer.js
@@ -3,7 +3,6 @@ async function generateMeteoContainer(currentValues, units) {
     const title = document.createElement('h2');
     const icon = document.createElement('img');
     const temp = document.createElement('h2');
-    const temperature = document.createElement('h2');
     const humidity = document.createElement('h2');
     const wind = document.createElement('h2');
 
@@ -11,7 +10,7 @@ async function generateMeteoContainer(currentValues, units) {
     title.textContent = container.getAttribute('data-city');
     icon.src = await getMeteoIcon(currentValues.weather_code);
     icon.alt = "Icono del clima";
-    temperature.textContent = `Temperatura: ${currentValues.temperature_2m} ${units.temperature_2m}`;
+    temp.textContent = `Temperatura: ${currentValues.temperature_2m} ${units.temperature_2m}`;
     humidity.textContent = `Humedad: ${currentValues.relative_humidity_2m} ${units.relative_humidity_2m}`;
     wind.textContent = `Viento: ${currentValues.wind_speed_10m} ${units.wind_speed_10m}`;
 
